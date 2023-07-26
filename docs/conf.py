97c3d9f8aa51a7d79c9c0d8e1414d00b36f1268a
joseph-palmer <22678593+joseph-palmer@users.noreply.github.com>
2023-07-26 14:54:28 +0100

    Adds basic site

---
+++ docs/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import sphinx_rtd_theme
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'RCTab'
+copyright = '2023, The Alan Turing Institute'
+author = 'The Alan Turing Institute'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_static_path = ['_static']
+
+html_logo = 'RCTab-hex.png'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': True,
+}
+
+def setup (app):
+    app.add_css_file('css/custom.css')
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. RCTab documentation master file, created by
+   sphinx-quickstart on Wed Jul 26 13:17:14 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+🚧 RCTab 🚧
+=================================
+
+RCTab is an Azure subscription management, reporting, and usage monitoring system.
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
