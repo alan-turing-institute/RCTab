bc839e540fbc1b3e6bb603ee288c397e0598e326
Iain-S <25081046+Iain-S@users.noreply.github.com>
2023-08-29 18:40:03 +0100

    Add sphinx requirements

--- docs/conf.py
+++ docs/conf.py
@@ -13,15 +13,36 @@ project = 'RCTab'
 copyright = '2023, The Alan Turing Institute'
 author = 'The Alan Turing Institute'
 
-sys.path.append(os.path.abspath('../rctab-infrastructure/src'))
-sys.path.append(os.path.abspath('../rctab-cli/rctab_cli'))
+#sys.path.append(os.path.abspath('../rctab-infrastructure/src'))
+#sys.path.append(os.path.abspath('../rctab-cli/rctab_cli'))
 
 os.environ["SPHYNX_AUTODOC_MODE"] = "true"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.autosummary"]
+extensions = [
+        "myst_parser",
+        "sphinx.ext.autodoc",
+        "sphinx.ext.napoleon",
+        "sphinx.ext.autosummary",
+        "sphinx.ext.intersphinx",
+        ]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    'mysub': ('https://sphinx-main-project.readthedocs.io/projects/ssp/en/latest/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+# We recommend adding the following config value.
+# Sphinx defaults to automatically resolve *unresolved* labels using all your Intersphinx mappings.
+# This behavior has unintended side-effects, namely that documentations local references can
+# suddenly resolve to an external location.
+# See also:
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#confval-intersphinx_disabled_reftypes
+intersphinx_disabled_reftypes = ["*"]
 
 autosummary_generate = True
 templates_path = ['_templates']
